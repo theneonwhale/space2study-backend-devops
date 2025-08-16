e51611f59ba6f6555a52bb689a0eba2858f31758
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2025-08-16 18:08:54 +0300

    code cleanup

--- terraform/modules/ec2_backend/output.tf
+++ terraform/modules/ec2_backend/output.tf
@@ -12,8 +12,3 @@ output "backend_security_group_id" {
   description = "Security group ID for backend EC2 instance"
   value       = aws_security_group.backend.id
 }
-
-output "backend_key_pair_name" {
-  description = "Key pair name for backend EC2 instance"
-  value       = aws_key_pair.space2study.key_name
-}
