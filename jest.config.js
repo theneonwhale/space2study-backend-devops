fdc5ad9d742ae7274cbbfde54ed56c2497ec2f24
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2025-08-16 15:11:41 +0300

    decrease test coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -18,10 +18,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
+      branches: 50,
+      functions: 40,
+      lines: 70,
+      statements: 70
     }
   },
   coverageReporters: ['html', 'lcov'],
