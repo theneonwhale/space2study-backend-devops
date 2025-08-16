cc45885d14cc7131c7f687e70cbf57e77a5bed48
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2025-08-16 20:41:00 +0300

    update linux version

--- terraform/modules/ec2_backend/main.tf
+++ terraform/modules/ec2_backend/main.tf
@@ -4,7 +4,7 @@ data "aws_ami" "amazon_linux_2023" {
   owners      = ["amazon"]
   filter {
     name   = "name"
-    values = ["al2023-ami-*-x86_64"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
   filter {
     name   = "architecture"
